334bce18743e60799069a980e606f5381be5b616
Mariam2189 <60200197+Mariam2189@users.noreply.github.com>
2020-06-05 15:12:03 +0300

    учебный проект: нас орда

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // 0='Дамблдор', 1='Волдеморт', 2='Доктор Стрендж', 3='Гарри Поттер'
-var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита','Вашингтон'];
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_LASTNAME = ['Верон', 'Вальц', 'Ирвинг', 'Нионго', 'да Марья', 'Мирабелла', 'Онопко', 'Топольницкая'];
 var WIZARD_COAT = ['rgb(215, 210, 55)', 'rgb(241, 43, 107', 'rgb(101, 137, 164)', 'rgb(0, 0, 0)', 'rgb(56, 159, 117)', 'rgb(146, 100, 161)'];
 var WIZARD_EYES = ['rgb(255, 255, 0)', 'rgb(50, 205, 50)', 'rgb(75, 0, 130)', 'rgb(178, 34, 34)', 'rgb(0, 0, 0)'];
@@ -21,24 +21,24 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
 
 var wizards = [
   {
+    name: WIZARD_NAMES[getRandomInt(WIZARD_NAMES.length - 1)] + " " + WIZARD_LASTNAME[getRandomInt(WIZARD_LASTNAME.length - 1)],
+    coatColor: WIZARD_COAT[getRandomInt(WIZARD_COAT.length - 1)],
+    eyesColor: WIZARD_EYES[getRandomInt(WIZARD_EYES.length -1)]
+  },
+  {
+    name: WIZARD_NAMES[getRandomInt(WIZARD_NAMES.length - 1)] + " " +  WIZARD_LASTNAME[getRandomInt(WIZARD_LASTNAME.length - 1)],
+    coatColor: WIZARD_COAT[getRandomInt(WIZARD_COAT.length - 1)],
+    eyesColor: WIZARD_EYES[getRandomInt(WIZARD_EYES.length - 1)]
+  },
+  {
+    name: WIZARD_NAMES[getRandomInt(WIZARD_NAMES.length - 1)] + " " +  WIZARD_LASTNAME[getRandomInt(WIZARD_LASTNAME.length - 1)],
+    coatColor: WIZARD_COAT[getRandomInt(WIZARD_COAT.length - 1)],
+    eyesColor: WIZARD_EYES[getRandomInt(WIZARD_EYES.length - 1)]
+  },
+  {
     name: WIZARD_NAMES[getRandomInt(WIZARD_NAMES.length - 1)] + " " +  WIZARD_LASTNAME[getRandomInt(3)],
     coatColor: WIZARD_COAT[getRandomInt(WIZARD_COAT.length - 1)],
-    eyesColor: WIZARD_EYES[getRandomInt(WIZARD_EYES.length -1)]
-  },
-  {
-    name: WIZARD_NAMES[getRandomInt(3)] + " " +  WIZARD_LASTNAME[getRandomInt(3)],
-    coatColor: WIZARD_COAT[getRandomInt(WIZARD_COAT.length - 1)],
-    eyesColor: WIZARD_EYES[getRandomInt(WIZARD_EYES.length -1)]
-  },
-  {
-    name: WIZARD_NAMES[getRandomInt(3)] + " " +  WIZARD_LASTNAME[getRandomInt(3)],
-    coatColor: WIZARD_COAT[getRandomInt(WIZARD_COAT.length - 1)],
-    eyesColor: WIZARD_EYES[getRandomInt(WIZARD_EYES.length -1)]
-  },
-  {
-    name: WIZARD_NAMES[getRandomInt(3)] + " " +  WIZARD_LASTNAME[getRandomInt(3)],
-    coatColor: WIZARD_COAT[getRandomInt(WIZARD_COAT.length - 1)],
-    eyesColor: WIZARD_EYES[getRandomInt(WIZARD_EYES.length -1)]
+    eyesColor: WIZARD_EYES[getRandomInt(WIZARD_EYES.length - 1)]
   }
 ];
 
